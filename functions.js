0603179f1f27345a8f304d34e6d33d47c6db5325
John Traut <johntraut@Johns-MBP.hitronhub.home>
2021-08-22 12:16:38 -0700

    major functions added

--- functions.js
+++ functions.js
@@ -1,9 +1,24 @@
 function test(pokeArray, divtoAppend) {
-  for (let i = 0; i < pokeArray.length; i++) {
-    let newgameDiv = document.createElement("div");
-    newgameDiv.setAttribute("id", `gameDiv  + ${i}`);
-    divtoAppend.appendChild(newgameDiv);
-  }
+  let item = pokeArray[Math.floor(Math.random() * pokeArray.length)];
+  console.log(item.name);
+  let newgameDiv = document.createElement("div");
+  newgameDiv.classList.add("TestClass");
+  let newgameImg = document.createElement("img");
+  applyStyles(newgameImg, newgameDiv, item);
+  divtoAppend.appendChild(newgameDiv);
+
+  // for (let i = 0; i < pokeArray.length; i++) {
+  //   let newgameDiv = document.createElement("div");
+  //   newgameDiv.setAttribute("id", `gameDiv ${i}`);
+  //   divtoAppend.appendChild(newgameDiv);
+  // }
+}
+
+function applyStyles(divToApplyStyles, divToAppend, itemInArray) {
+  divToApplyStyles.setAttribute("id", `gameDiv ${itemInArray.name}`);
+  divToApplyStyles.classList.add("GameImageClass");
+  divToApplyStyles.src = `${itemInArray.src}`;
+  divToAppend.appendChild(divToApplyStyles);
 }
 
 export { test };
--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 import { test } from "./functions.js";
 
-let pokemonGamesArray = ["red", "blue", "yellow"];
+let pokemonGamesArray = [
+  { name: "red", src: "./assets/Red.png" },
+  { name: "blue", src: "./assets/Blue.png" },
+  { name: "yellow", src: "./assets/Yellow.jpeg" },
+  //   "gold",
+  //   "silver",
+  //   "crystal",
+  //   "ruby",
+  //   "sapphire",
+  //   "emerald",
+  //   "fire red",
+  //   "leaf green",
+  //   "diamond",
+  //   "pearl",
+  //   "platnium",
+  //   "HG",
+  //   "SS",
+  //   "black",
+  //   "white",
+  //   "black 2",
+  //   "while 2",
+  //   "x",
+  //   "y",
+  //   "sun",
+  //   "moon",
+];
 let gameImg = document.getElementsByClassName("game-img")[0];
-console.log(gameImg);
 
 test(pokemonGamesArray, gameImg);
